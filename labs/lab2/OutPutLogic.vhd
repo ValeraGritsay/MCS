47f5cdc7488d5516550bbed48ccea5d9054384f0
ValeraGritsay <129192864+ValeraGritsay@users.noreply.github.com>
2023-05-02 13:56:37 +0300

    Add files via upload

---
+++ labs/lab2/OutPutLogic.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:11:14 05/18/2022 
+-- Design Name: 
+-- Module Name:    Out_Logic_intf - Out_Logic_arch 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity Out_Logic_intf is
+Port ( IN_BUS : in  std_logic_vector(2 downto 0);
+       OUT_BUS : out  std_logic_vector(7 downto 0)
+	  );
+end Out_Logic_intf;
+
+architecture Out_Logic_arch of Out_Logic_intf is
+
+begin
+ OUT_BUS(0) <= (not(IN_BUS(2)) and not(IN_BUS(1)) and not(IN_BUS(0)));
+	 OUT_BUS(1) <= (not(IN_BUS(2)) and IN_BUS(1) and not(IN_BUS(0)));
+	 OUT_BUS(2) <= (IN_BUS(2) and not(IN_BUS(1)) and not(IN_BUS(0)));
+	 OUT_BUS(3) <= (IN_BUS(2) and IN_BUS(1) and not(IN_BUS(0)));
+    OUT_BUS(4) <= (IN_BUS(2) and IN_BUS(1) and IN_BUS(0));
+	 OUT_BUS(5) <= (IN_BUS(2) and not(IN_BUS(1)) and IN_BUS(0));
+	 OUT_BUS(6) <= (not(IN_BUS(2)) and IN_BUS(1) and IN_BUS(0));
+	 OUT_BUS(7) <= (not(IN_BUS(2)) and not(IN_BUS(1)) and IN_BUS(0));
+
+end Out_Logic_arch;
---
+++ labs/lab2/Transition_Logic.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:21:12 05/18/2022 
+-- Design Name: 
+-- Module Name:    Transition_Logic_intf - Transition_Logic_arch 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Transition_Logic_intf is
+ Port ( CUR_STATE : in  std_logic_vector(2 downto 0);
+        MODE : in  std_logic;
+        NEXT_STATE : out  std_logic_vector(2 downto 0)
+	   );
+end Transition_Logic_intf;
+
+architecture Transition_Logic_arch of Transition_Logic_intf is
+
+begin
+    NEXT_STATE(0) <= (MODE and not(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and not(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and not(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and not(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0)));
+							
+	 NEXT_STATE(1) <= (MODE and not(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and (CUR_STATE(1)) and(CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and not (CUR_STATE(2)) and (CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and not(CUR_STATE(2)) and not(CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and not(CUR_STATE(2)) and (CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and not(CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0)));	
+
+    NEXT_STATE(2) <= (MODE and not(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and (CUR_STATE(1)) and(CUR_STATE(0))) or
+							(MODE and(CUR_STATE(2)) and (CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(MODE and (CUR_STATE(2)) and not(CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and not(CUR_STATE(2)) and(CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and not(CUR_STATE(1)) and not (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and not(CUR_STATE(1)) and (CUR_STATE(0))) or
+							(not(MODE) and(CUR_STATE(2)) and(CUR_STATE(1)) and not (CUR_STATE(0)));		
+
+end Transition_Logic_arch;
